2ecd37609e9af53156bb59ebf55de2e2cac109dd
juthomas <juthomas@student.42.fr>
2022-09-22 13:36:15 +0200

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
+import { Card, Image } from '@mantine/core';
 import * as React from 'react';
 import './style.css';
+import background from './imgs/card-reward-01.png';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {/* <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p> */}
+      <Card shadow="sm" radius="md" p="lg" withBorder>
+        <Card.Section>
+          <Image src={background} />
+        </Card.Section>
+      </Card>
     </div>
   );
 }
